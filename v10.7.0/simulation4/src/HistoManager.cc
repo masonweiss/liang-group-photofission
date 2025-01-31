e4a5873d3a41b75c14f82631918230e28feeb0a5
Mason Weiss <mw103@rice.edu>
2025-01-30 22:55:52 -0600

    fix

--- v10.7.0/simulation4/src/HistoManager.cc
+++ v10.7.0/simulation4/src/HistoManager.cc
@@ -29,10 +29,10 @@ void HistoManager::Book()
   analysisManager->SetActivation(true);     //enable inactivation of histograms
   
   // Define histograms start values
-  const G4int kMaxHisto = 31;
+  const G4int kMaxHisto = 37;
   const G4String id[] = {"0","1","2","3","4","5","6","7","8","9",
                          "10","11","12","13","14","15","16","17","18","19",
-			 "20","21","22","23","24","25","26","27","28","29","30" };
+			 "20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36"};
   const G4String title[] = 
        { "dummy",                                                        //0
          "total energy deposit",                                         //1
