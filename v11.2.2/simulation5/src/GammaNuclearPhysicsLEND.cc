b7c695f14249a18fc20c38f0953847f1713942b2
Mason Weiss <mw103@rice.edu>
2025-02-20 21:25:13 -0600

    test LEND

--- v11.2.2/simulation5/src/GammaNuclearPhysicsLEND.cc
+++ v11.2.2/simulation5/src/GammaNuclearPhysicsLEND.cc
@@ -57,7 +57,7 @@ void GammaNuclearPhysicsLEND::ConstructProcess()
    G4ProcessManager* pManager = G4Gamma::Gamma()->GetProcessManager();
    //
    G4HadronInelasticProcess* process
-       = new G4HadronInelasticProcess("photonNuclear", G4Gamma::Definition());
+       = new G4HadronInelasticProcess("photonNuclear_low", G4Gamma::Definition());
    process->AddDataSet( new G4PhotoNuclearCrossSection );
    //
    G4LENDorBERTModel* lend = new G4LENDorBERTModel(G4Gamma::Gamma());
