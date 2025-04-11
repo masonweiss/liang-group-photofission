e8c3e0b826388d9e59f19983916d83867f7befaf
Mason Weiss <mw103@rice.edu>
2025-04-11 11:06:06 -0500

    100 events with 1m gammas

--- v11.2.2/simulation5/src/PrimaryGeneratorAction.cc
+++ v11.2.2/simulation5/src/PrimaryGeneratorAction.cc
@@ -43,7 +43,7 @@
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
 {
-  G4int n_particle = 1000;
+  G4int n_particle = 1000000;
   fParticleGun  = new G4ParticleGun(n_particle);
   
   // default particle kinematic
